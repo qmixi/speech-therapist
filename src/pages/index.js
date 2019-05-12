1dbb60c7ffb665c8eeb7123f1c6cb3bbf174e878
Piotr Kumorek <piotr.kumorek@netguru.co>
2019-05-12 14:49:37 +0200

    feat: integrate gatsby with graphcms & display quotation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { arrayOf, shape } from 'prop-types';
 
-import SEO from 'components/seo';
+import SEO from 'components/Seo';
+import Quottion from 'components/Quotation';
 
-const IndexPage = () => (
+const IndexPage = ({
+  data: {
+    therapist: { quotations },
+  },
+}) => (
   <div>
     <SEO title="" keywords={[`logopeda`, `neurologopeda`, `Toruń`]} />
+    <Quottion data={quotations} />
     <h1>Logopeda Toruń</h1>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 );
+
+export const query = graphql`
+  query {
+    therapist {
+      quotations {
+        id
+        text
+        author
+      }
+    }
+  }
+`;
+
+IndexPage.propTypes = {
+  data: shape({
+    therapist: shape({
+      quotations: arrayOf(shape({})),
+    }),
+  }).isRequired,
+};
 
 export default IndexPage;
